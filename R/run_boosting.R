2a482502f5480910b0dc73dd44a4ffa3a2426e6a
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-09-25 12:35:35 +0200

    Boosting recovers same parameters as nlm

--- R/run_boosting.R
+++ R/run_boosting.R
@@ -5,11 +5,31 @@ run_boosting <- function() {
   X <- simulated_data$design_matrices$X
   Z <- simulated_data$design_matrices$Z
 
+  # center Z for correct boosting
+  Z <- scale(Z, center=T, scale=F)
+
   # Our loss function
-  minus_FHT_loglikelihood <- data_to_FHT_loss_function(X, Z, times, delta)
   # usage: minus_FHT_loglikelihood(list(beta=beta, gamma=gamma))
 
-  m_stop <- 100
+  beta_true <- c(0.5, 1)       # the true beta!
+  gamma_true <- c(-0.2, -0.2)
+
+  ### SANITY CHECK -> does nlm recover the parameters? ###
+  minus_FHT_loglikelihood_nlm <- data_to_optimizable_function(X, Z, times, delta)
+
+  p <- dim(X)[2]
+  d <- dim(Z)[2]
+
+  # Run optimization
+  initial_parameters <- runif(p+d, min=0.1, max=0.5)
+  nlm_result <- nlm(minus_FHT_loglikelihood_nlm, initial_parameters)
+  #sum(abs(nlm_result$estimate - c(beta_true, gamma_true)))
+  ### END SANITY CHECK ###
+
+  beta_from_nlm <- nlm_result$estimate[1:2]
+  gamma_from_nlm <- nlm_result$estimate[3:4]
+
+  m_stop <- 10
 
   N <- dim(X)[1]
 
@@ -23,8 +43,8 @@ run_boosting <- function() {
 
   nu <- 0.1
 
-  N <- dim(X)[1]
-  p <- dim(X)[2]
+  N <- dim(Z)[1]
+  p <- dim(Z)[2]
 
   # Standardize X -- use scale instead!! (?)
 
@@ -35,15 +55,16 @@ run_boosting <- function() {
   #   X[, j] <- (X[, j] - X_colmeans[j])/X_sds[j]
   # }
 
-  beta_true <- c(0.5, 1)       # the true beta!
-  gamma_true <- c(-0.2, -0.2)
+  # "make" loss function after scaling Z
+  minus_FHT_loglikelihood <- data_to_FHT_loss_function(X, Z, times, delta)
+  #minus_FHT_loglikelihood(list(beta=beta_true, gamma=gamma_true))
 
   gamma_hat <- matrix(NA, nrow=m_stop, ncol=p)
   gamma_hat[1, ] <- c(0, 0)
-  gradient <- matrix(NA, nrow=m_stop, ncol=N)
-  parameter_list <- list(beta=beta_true, gamma=gamma_hat[1, ])
+  negative_gradient <- matrix(NA, nrow=m_stop, ncol=N)
+  parameter_list <- list(beta=beta_from_nlm, gamma=gamma_hat[1, ])
   #losses[1, ] <- FHT_componentwise_minus_loglikelihood_with_parameters(parameter_list, X, Z, times, delta)
-  gradient[1, ] <- - FHT_componentwise_loss_function_derivative_mu(parameter_list, X, Z, times, delta)
+  negative_gradient[1, ] <- - FHT_componentwise_loss_function_derivative_mu(parameter_list, X, Z, times, delta)
   loss <- rep(NA, m_stop)
   loss[1] <- minus_FHT_loglikelihood(parameter_list)
 
@@ -51,45 +72,20 @@ run_boosting <- function() {
   u_hats <- matrix(NA, nrow=p, ncol=N)
   gamma_hats <- rep(NA, p)
   for (m in 2:m_stop) {
-    u <- gradient[(m-1), ]
+    u <- negative_gradient[(m-1), ]
     for (j in 1:p) {
-      X_j <- X[,j]
-      S <- X_j %*% solve(t(X_j) %*% X_j) %*% t(X_j)
-      gamma_hats[j] <- solve(t(X_j) %*% X_j) %*% t(X_j) %*% u
-      u_hats[j, ] <- X_j * gamma_hats[j]
-      #u_hats[j, ] <- S %*% u
+      Z_j <- Z[,j]
+      gamma_hats[j] <- solve(t(Z_j) %*% Z_j) %*% t(Z_j) %*% u
+      u_hats[j, ] <- Z_j * gamma_hats[j]
       u_hats_rss[j] <- sum((u_hats[j, ] - u)^2) / N
     }
     best_p <- which.min(u_hats_rss)
     gamma_hat[m, best_p] <- nu*gamma_hats[best_p]
     gamma_hat[m, -best_p] <- 0
-    parameter_list <- list(beta=beta_true, gamma=colSums(gamma_hat[1:m, ]))
-    gradient[m, ] <- - FHT_componentwise_loss_function_derivative_mu(parameter_list, X, Z, times, delta)
+    parameter_list <- list(beta=beta_from_nlm, gamma=colSums(gamma_hat[1:m, ]))
+    negative_gradient[m, ] <- - FHT_componentwise_loss_function_derivative_mu(parameter_list, X, Z, times, delta)
     loss[m] <- minus_FHT_loglikelihood(parameter_list)
+    m <- m + 1
   }
 
-
-
-  link_function <- FHT_link_function(X, Z)
-  FHT_parameters <- link_function(beta_hat, gamma_hat)
-  y0s <- FHT_parameters$y0
-  y0 <- 1
-  mus <- FHT_parameters$mu
-  sigma2 <- 1
-  p <- 2
-  N <- 1000
-
-  u <- - sapply(mus, function(mu) { loss_function_derivative_mu(y0, mu, sigma2, times, delta) })
-
-  u_hats <- rep(NA, p)
-  for (column in 1:p) {
-    X_subset <- X[,column]
-    u_hat <- X_subset %*% solve(t(X_subset) %*% X_subset) %*% t(X_subset) %*% u
-    rss <- sum((u_hat - u)^2) / N
-    u_hats[column] <- rss
-  }
-  best_p <- which.min(u_hats)
-
-  M <- 1000
-  return(loss)
 }
