b82b7a203c63312df6c2b3876b1b1cc420ddc5d5
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-10-23 10:58:36 +0200

    Add parametric functions

---
+++ R/FHT_parametric_cumulative_hazard.R
@@ -0,0 +1,3 @@
+FHT_parametric_cumulative_hazard <- function(times, mu, y0) {
+  return(-log(pnorm((mu*times + y0)/sqrt(times)) - exp(-2*y0*mu)*pnorm((mu*times - y0))/sqrt(times)))
+}
---
+++ R/FHT_parametric_survival.R
@@ -0,0 +1,3 @@
+FHT_parametric_survival <- function(times, mu, y0) {
+  return(pnorm((mu*times + y0)/sqrt(times)) - exp(-2*y0*mu)*pnorm((mu*times - y0))/sqrt(times))
+}
