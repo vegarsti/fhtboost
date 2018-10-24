2d55023a5f171f52a11b6b458d79fe02b0cf5418
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-10-24 11:17:37 +0200

    Move part of loglikelihood out into own function

--- R/FHT_componentwise_minus_loglikelihood_with_parameters.R
+++ R/FHT_componentwise_minus_loglikelihood_with_parameters.R
@@ -3,10 +3,6 @@ FHT_componentwise_minus_loglikelihood_with_parameters <- function(beta_, gamma_,
   d <- dim(Z)[2]
   y0 <- exp(X %*% beta_)
   mu <- Z %*% gamma_
-  log_f <- log(y0) - 0.5*log(2*pi*times^3) - (y0 + mu*times)^2/(2*times)
-  S <- pnorm((mu*times + y0)/sqrt(times)) - exp(-2*y0*mu)*pnorm((mu*times - y0))/sqrt(times)
-  log_S <- log(S)
-  #loglikelihood_vector <- delta*log_f + (1-delta)*log_S
-  loglikelihood_vector <- ifelse(delta, log_f, log_S)
-  return(loglikelihood_vector)
+  loglikelihood_vector <- FHT_loglikelihood_with_y0_mu(y0, mu, times, delta)
+  return(-loglikelihood_vector)
 }
--- R/FHT_minus_loglikelihood_with_all_parameters.R
+++ R/FHT_minus_loglikelihood_with_all_parameters.R
@@ -1,5 +1,5 @@
 FHT_minus_loglikelihood_with_all_parameters <- function(beta_, gamma_, X, Z, times, delta) {
-  loglikelihood_vector <- FHT_componentwise_minus_loglikelihood_with_parameters(beta_, gamma_, X, Z, times, delta)
-  loglikelihood_value <- sum(loglikelihood_vector)
-  return(-loglikelihood_value)
+  minus_loglikelihood_vector <- FHT_componentwise_minus_loglikelihood_with_parameters(beta_, gamma_, X, Z, times, delta)
+  minus_loglikelihood_value <- sum(minus_loglikelihood_vector)
+  return(minus_loglikelihood_value)
 }
