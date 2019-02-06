76cf3c9ac107353f1ce0405948c28cd50c76cee9
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-02-06 14:45:55 +0100

    Make Brier evaluation more efficient

--- R/brier_r2.R
+++ R/brier_r2.R
@@ -1,6 +1,6 @@
-brier_r2 <- function(times, delta, estimated_y0s, estimated_mus, null_y0, null_mu) {
-  non_null <- brier_score_on_censored_data(times, delta, estimated_y0s, estimated_mus)
-  null <- brier_score_on_censored_data(times, delta, null_y0, null_mu)
+brier_r2 <- function(times, delta, estimated_y0s, estimated_mus, null_y0, null_mu, number_of_time_points=100) {
+  non_null <- brier_score_on_censored_data(times, delta, estimated_y0s, estimated_mus, number_of_time_points=number_of_time_points)
+  null <- brier_score_on_censored_data(times, delta, null_y0, null_mu, number_of_time_points=number_of_time_points)
   r2 <- 1 - non_null$brier_score/null$brier_score
-  return(list(r2_times=non_null$brier_times, r2=r2))
+  return(data.frame(times=non_null$brier_times, r2=r2, brier_score=non_null$brier_score, brier_null_score=null$brier_score))
 }
--- R/brier_score_on_censored_data.R
+++ R/brier_score_on_censored_data.R
@@ -1,6 +1,5 @@
-brier_score_on_censored_data <- function(times, delta, estimated_y0s, estimated_mus) {
-  time_points <- 1000
-  brier_times <- seq(from=0.1, to=max(times), length.out=time_points)
+brier_score_on_censored_data <- function(times, delta, estimated_y0s, estimated_mus, number_of_time_points=1000) {
+  brier_times <- seq(from=0.1, to=max(times), length.out=number_of_time_points)
   brier_scores <- sapply(brier_times, function(time) {
     brier_score_with_censoring(time, times, delta, estimated_y0s, estimated_mus)
   })
