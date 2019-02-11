b7dfa2755a3a89b094337f747fc7477cbe489bec
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-02-11 11:12:13 +0100

    Add script to source to run externally

---
+++ R/run_on_other_computer.R
@@ -0,0 +1,27 @@
+library(devtools)
+library(foreach)
+library(readr)
+library(doParallel)
+library(fhtboost)
+
+# Configuration
+seeds <- 167:200
+
+no_cores <- detectCores() - 1
+registerDoParallel(cores=no_cores)
+cl <- makeCluster(no_cores)
+directory <- "./simulations/"
+N <- 500
+N_test <- 1000
+setup_type <- 'huge_clinical'
+add_noise <- FALSE
+K <- 5
+K_fold_repetitions <- 5
+M <- 80
+TEST_SEED <- 9000
+
+foreach(seed=seeds) %dopar% {
+  run_CV_and_write_to_file(N, setup_type, add_noise, seed, K, K_fold_repetitions, directory, M)
+  estimate_model_and_validate_and_write_to_file(N, setup_type, add_noise, seed, directory)
+}
+stopCluster(cl)
