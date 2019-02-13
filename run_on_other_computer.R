edfd7e625864fbc19db05b52d20dd8fcab2b2d86
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-02-13 12:54:04 +0100

    Update scripts

--- run_on_other_computer.R
+++ run_on_other_computer.R
@@ -5,7 +5,8 @@ library(doParallel)
 library(fhtboost)
 
 # Configuration
-seeds <- 167:200
+#seeds <- 167:200
+seeds <- c(1, 13, 26, 27, 41, 45, 49, 85, 88, 94, 99, 121, 150, 159, 162, 165, 166, 172, 174, 195, 196, 208, 212, 224, 227, 240, 253, 260, 261, 263, 269, 271, 289, 291, 292, 293, 303, 310, 320, 322, 357, 367, 371, 375, 379, 399, 402, 405, 406, 407, 420, 433, 443, 445, 449, 457, 466, 495)
 
 no_cores <- detectCores() - 1
 registerDoParallel(cores=no_cores)
@@ -17,7 +18,7 @@ setup_type <- 'huge_clinical'
 add_noise <- FALSE
 K <- 5
 K_fold_repetitions <- 5
-M <- 80
+M <- 130
 TEST_SEED <- 9000
 boost_intercepts_continually <- FALSE
 
--- summarize_dataset.R
+++ summarize_dataset.R
@@ -4,14 +4,15 @@ load_all()
 
 # Configuration
 seeds <- 1:500
-not_seeds <- c(15, 17, 24, 30, 57, 58, 63, 68, 70, 78, 79, 90, 93, 95, 103, 110, 113, 123, 140, 141, 144, 149, 160, 170, 171, 179, 181, 183, 184, 189, 193, 200, 201, 206, 213, 217, 218, 236, 248, 249, 251, 256, 276, 278, 308, 314, 316, 317, 337, 338, 344, 349, 361, 366, 376, 381, 382, 391, 394, 396, 404, 421, 427, 428, 435, 459, 461, 463, 464, 467, 472, 490, 491, 492, 494, 500)
+not_seeds <- c(15, 17, 24, 30, 57, 58, 63, 68, 70, 78, 79, 90, 93, 95, 103, 110, 113, 123, 140, 141, 144, 149, 160, 170, 171, 179, 181, 183, 184, 189, 193, 200, 201, 206, 213, 217, 218, 236, 238, 241, 244, 247, 248, 249, 250, 251, 256, 276, 278, 308, 314, 316, 317, 325, 337, 338, 344, 349, 361, 366, 376, 381, 382, 391, 394, 396, 404, 421, 427, 428, 435, 459, 461, 463, 464, 467, 472, 490, 491, 492, 494, 500)
 seeds <- seeds[-not_seeds]
 
 B <- length(seeds)
 
-input_directory <- "../dataset/"
+base_input_directory <- "../dataset/"
+algorithm <- 'non-cyclic-intercept'
+input_directory <- paste(base_input_directory, algorithm, sep='')
 output_directory <- '../simulation-results/'
-algorithm <- 'non-cyclic'
 scenario <- 'uncorrelated'
 
 informative_p <- 35
@@ -58,115 +59,60 @@ total_gamma <- total_gamma[-1, ]
 
 ### PROCESSING RESULTS ####
 ## BETA
-result_beta <- t(apply(total_beta, 2, function(column) {
-  non_null <- column != 0
-  chosen_parameters <- sum(non_null)
-  informative_chosen <- sum(non_null[1:informative_p])
-  non_informative_chosen <- sum(non_null[(informative_p+1):p])
-  c(chosen_parameters, informative_chosen, non_informative_chosen)
-}))
-result_beta <- data.frame(beta_n=result_beta[, 1], beta_informative=result_beta[, 2], beta_non_informative=result_beta[, 3])
+summarize_variable_selection <- function(all_parameter_results, p, informative_p) {
+  non_informative_p <- p - informative_p
+  result <- t(apply(all_parameter_results, 2, function(column) {
+    non_null <- column != 0
+    chosen_parameters <- sum(non_null)
+    informative_chosen <- sum(non_null[1:informative_p])
+    non_informative_chosen <- sum(non_null[(informative_p+1):p])
+    c(chosen_parameters, informative_chosen, non_informative_chosen)
+  }))
+  P <- informative_p
+  N <- non_informative_p
+  result <- data.frame(TP=result[, 2], FP=result[, 3])
+  result$FN <- informative_p - result$TP
+  result$TN <- non_informative_p - result$FP
+  result$sensitivity <- result$TP/P # TPR
+  result$specificity <- result$TN/N # TNR
+  result$accuracy <- (result$TN+result$TP)/(N+P) # TNR
+  result$FPR <- result$FP/N # false positive rate
+  result$FNR <- result$FN/P # false negative rate
+  #result_beta$FDR <- result_beta$FP/(result_beta$FP+result_beta$TP) # false discovery rate
+  result$FOR <- result$FN/(result$FN+result$TN) # false omission rate
+  result$NPV <- result$TN/(result$FN+result$TN) # false omission rate
 
-non_null_parameter_count_beta <- apply(total_beta, 1, function(row) length(non_null_parameters(row)))# / B
-number_of_chosen_parameters_beta <- sum(non_null_parameter_count_beta)
-total_number_of_parameters_beta <- p * B
-total_number_of_informative_parameters_beta <- informative_p * B
-total_number_of_non_informative_parameters_beta <- non_informative_p * B
+  result_mean <- as.data.frame(colMeans(result))
+  result_sd <- as.data.frame(apply(result, 2, sd))
+  result_summary <- as.data.frame(cbind(result_mean, result_sd))
+  colnames(result_summary) <- c("mean", "sd")
+  result_summary$rownames <- c("TP", "FP", "FN", "TN", "sensitivity", "specificity", "accuracy", "FPR", "FNR", "FOR", "NPV")
+  result_summary <- result_summary[-c(1:4), ]
+  return(result_summary)
+}
 
-
-informative_parameters <- non_null_parameter_count_beta[1:informative_p]
-non_informative_parameters <- non_null_parameter_count_beta[(informative_p+1):p]
-
-number_of_chosen_informative_parameters_beta <- sum(informative_parameters)
-number_of_chosen_non_informative_parameters_beta <- sum(non_informative_parameters)
-
-N <- total_number_of_non_informative_parameters_beta
-P <- total_number_of_informative_parameters_beta
-TP <- number_of_chosen_informative_parameters_beta
-FN <- total_number_of_informative_parameters_beta - number_of_chosen_informative_parameters_beta
-FP <- number_of_chosen_non_informative_parameters_beta
-TN <- total_number_of_non_informative_parameters_beta - number_of_chosen_non_informative_parameters_beta
-
-FPR <- FP/N # false positive rate
-FNR <- FN/P # false negative rate
-FDR <- FP/(FP+TP)
-FOR <- FN/(FN+TN)
-precision <- PPV <- TP/(TP+FP) # positive predictive value
-sensitivity <- TPR <- TP/P # true positive rate
-specificity <- TNR <- TN/N # true negative rate
-accuracy <- (TP+TN)/(P+N)
-confusion_matrix_beta <- matrix(c(TP, FN, FP, TN), nrow=2)
-
-beta_results_df <- data.frame(
-  N=N,P=P,TP=TP,FN=FN,FP=FP,TN=TN,
-  false_positive_rate=FPR,false_negative_rate=FNR,false_discovery_rate=FDR,false_omission_rate=FOR,
-  precision=precision,sensitivity=sensitivity,specificity=specificity,accuracy=accuracy
-)
-
-
+result_beta_summary <- summarize_variable_selection(total_beta, p, informative_p)
 filename <- paste(algorithm, scenario, 'beta', sep='_')
 full_filename <- paste(output_directory, filename, '.csv', sep='')
-write.csv(beta_results_df, full_filename)
-
+write.csv(result_beta_summary[, -3], full_filename, row.names=result_beta_summary$rownames)
 
 
 # GAMMA
-
-result_gamma <- t(apply(total_gamma, 2, function(column) {
-  non_null <- column != 0
-  chosen_parameters <- sum(non_null)
-  informative_chosen <- sum(non_null[1:informative_d])
-  non_informative_chosen <- sum(non_null[(informative_d+1):d])
-  c(chosen_parameters, informative_chosen, non_informative_chosen)
-}))
-result_gamma <- data.frame(gamma_n=result_gamma[, 1], gamma_informative=result_gamma[, 2], gamma_non_informative=result_gamma[, 3])
-
-
-non_null_parameter_count_gamma <- apply(total_gamma, 1, function(row) length(non_null_parameters(row)))
-number_of_chosen_parameters_gamma <- sum(non_null_parameter_count_gamma)
-total_number_of_parameters_gamma <- d * B
-total_number_of_informative_parameters_gamma <- informative_d * B
-total_number_of_non_informative_parameters_gamma <- non_informative_d * B
-
-
-informative_parameters_gamma <- non_null_parameter_count_gamma[1:informative_d]
-non_informative_parameters_gamma <- non_null_parameter_count_gamma[(informative_d+1):d]
-
-number_of_chosen_informative_parameters_gamma <- sum(informative_parameters_gamma)
-number_of_chosen_non_informative_parameters_gamma <- sum(non_informative_parameters_gamma)
-
-N <- total_number_of_non_informative_parameters_gamma
-P <- total_number_of_informative_parameters_gamma
-TP <- number_of_chosen_informative_parameters_gamma
-FN <- total_number_of_informative_parameters_gamma - number_of_chosen_informative_parameters_gamma
-FP <- number_of_chosen_non_informative_parameters_gamma
-TN <- total_number_of_non_informative_parameters_gamma - number_of_chosen_non_informative_parameters_gamma
-
-FPR <- FP/N # false positive rate
-FNR <- FN/P # false negative rate
-FDR <- FP/(FP+TP)
-FOR <- FN/(FN+TN)
-precision <- PPV <- TP/(TP+FP) # positive predictive value
-sensitivity <- TPR <- TP/P # true positive rate
-specificity <- TNR <- TN/N # true negative rate
-accuracy <- (TP+TN)/(P+N)
-confusion_matrix_gamma <- matrix(c(TP, FN, FP, TN), nrow=2)
-
-gamma_results_df <- data.frame(
-  N=N,P=P,TP=TP,FN=FN,FP=FP,TN=TN,
-  false_positive_rate=FPR,false_negative_rate=FNR,false_discovery_rate=FDR,false_omission_rate=FOR,
-  precision=precision,sensitivity=sensitivity,specificity=specificity,accuracy=accuracy
-)
-
-
+result_gamma_summary <- summarize_variable_selection(total_gamma, d, informative_d)
 filename <- paste(algorithm, scenario, 'gamma', sep='_')
 full_filename <- paste(output_directory, filename, '.csv', sep='')
-write.csv(gamma_results_df, full_filename)
-
+write.csv(result_gamma_summary[, -3], full_filename, row.names=result_gamma_summary$rownames)
 
 
 ## ALL (results from each run)
-all_results <- cbind(result_beta, result_gamma, total_summary)
-filename <- paste(algorithm, scenario, 'all', sep='_')
+summary_means <- colMeans(total_summary)
+summary_sd <- apply(total_summary, 2, sd)
+summary_min <- apply(total_summary, 2, min)
+summary_max <- apply(total_summary, 2, max)
+summary <- cbind(summary_means, summary_sd, summary_min, summary_max)
+colnames(summary) <- c("mean", "sd", "min", "max")
+summary <- as.data.frame(summary)
+
+filename <- paste(algorithm, scenario, 'summary', sep='_')
 full_filename <- paste(output_directory, filename, '.csv', sep='')
-write.csv(all_results, full_filename)
+write.csv(summary, full_filename)
