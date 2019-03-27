acbbc639e0b3ff5e6085ae95ad146b9f4c2a28cf
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-03-27 11:19:45 +0100

    Update Oberthur scripts

--- run_oberthuer_remote.R
+++ run_oberthuer_remote.R
@@ -19,17 +19,23 @@ no_cores <- detectCores() - 1
 registerDoParallel(cores=no_cores)
 cl <- makeCluster(no_cores)
 
+# OPTIONS
+K_fold_repetitions <- 10
+K <- 5
+boost_intercepts_continually <- FALSE
+run_clinical <- FALSE
+run_both <- FALSE
+run_genetic <- TRUE
+
 seeds <- 1:100
 
 foreach(seed=seeds) %dopar% {
   set.seed(seed)
   seed_string <- formatC(seed, width=2, flag="0")
 
-
-
   # Divide into test and train. test approx 1/3
-  K <- 3
-  folds <- create_folds_stratified(delta, K)
+  num_folds <- 3
+  folds <- create_folds_stratified(delta, num_folds)
   test_indices <- sort(folds[[1]])
   train_indices <- sort(c(folds[[2]], folds[[3]]))
 
@@ -60,20 +66,6 @@ foreach(seed=seeds) %dopar% {
     ylab="Kaplan-Meier estimated survival probability", ylim=c(0, 1)
   )
   dev.off()
-
-
-  # find intercepts to start with
-  K_fold_repetitions <- 10
-  K <- 5
-  boost_intercepts_continually <- FALSE
-
-  ## RUN CV
-
-  run_clinical <- TRUE
-  run_both <- FALSE
-
-
-
 
 
   # Both
@@ -182,19 +174,20 @@ foreach(seed=seeds) %dopar% {
       boost_intercepts_continually=boost_intercepts_continually
     )
 
+    boosting_type <- "clinical"
 
     ### WRITE CV RESULT TO FILE
-    full_filename <- paste0(directory, seed_string, "_", "clinical", "_", "loglik.csv")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "loglik.csv")
     write.csv(CV_result_clinical$CV_errors_K_loglik, file=full_filename, row.names=FALSE)
-    full_filename <- paste0(directory, seed_string, "_", "clinical", "_", "deviance.csv")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "deviance.csv")
     write.csv(CV_result_clinical$CV_errors_K_deviance, file=full_filename, row.names=FALSE)
 
 
     ## READ CV RESULT FROM FILE
     seed_string <- formatC(seed, width=2, flag="0")
-    full_filename <- paste0(directory, seed_string, "_", "clinical", "_", "loglik.csv")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "loglik.csv")
     logliks <- read.csv(full_filename)
-    full_filename <- paste0(directory, seed_string, "_", "clinical", "_", "deviance.csv")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "deviance.csv")
     deviances <- read.csv(full_filename)
 
     ### POST PROCESSING AND PLOTTING
@@ -202,7 +195,7 @@ foreach(seed=seeds) %dopar% {
     ylims <- c(min(apply(logliks, 2, min)), max(apply(logliks, 2, max)))
     m_stop_from_CV <- which.min(rowMeans(logliks))
     #m_stop_from_CV <- which.max(rowMeans(deviances))
-    full_filename <- paste0(directory, seed_string, "_", "clinical", "_", "loglik.pdf")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "loglik.pdf")
     pdf(full_filename, width=12, height=6)
     plot(rowMeans(logliks), typ='l', ylim=ylims, ylab="Negative log-likelihood", xlab="Boosting iteration")
     for (k in 1:K_fold_repetitions) {
@@ -218,13 +211,14 @@ foreach(seed=seeds) %dopar% {
     )
     dev.off()
 
-    result <- boosting_run(
+    result <- cyclic_boosting_run(
       times=times_train,
       delta=delta_train,
       X=X_train,
       Z=Z_train,
-      m_stop=m_stop_from_CV,
-      boost_intercepts_continually=FALSE,
+      m_stop_y0=1,
+      m_stop_mu=m_stop_from_CV,
+      boost_intercepts_continually=boost_intercepts_continually,
       should_print=FALSE
     )
 
@@ -235,11 +229,11 @@ foreach(seed=seeds) %dopar% {
 
     betas <- data.frame(cbind(non_null_parameters(beta_hat) - 1, beta_hat[non_null_parameters(beta_hat)]))
     names(betas) <- c("j", "beta_j")
-    full_filename <- paste0(directory, seed_string, "_", "clinical", "_", "beta.csv")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "beta.csv")
     write.csv(betas, file=full_filename, row.names=FALSE)
     gammas <- data.frame(cbind((1:length(gamma_hat)) - 1, gamma_hat))
     names(gammas) <- c("j", "gamma_j")
-    full_filename <- paste0(directory, seed_string, "_", "clinical", "_", "gamma.csv")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "gamma.csv")
     write.csv(gammas, file=full_filename, row.names=FALSE)
 
 
@@ -261,7 +255,102 @@ foreach(seed=seeds) %dopar% {
       loglikelihood=test_loglikelihood,
       deviance=test_difference_of_deviance
     )
-    full_filename <- paste0(directory, seed_string, "_", "clinical", "_", "test_result.csv")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "test_result.csv")
+    write.csv(loglikelihood_df, file=full_filename, row.names=FALSE)
+  }
+
+
+  if (run_genetic) {
+    # Genetic
+    M_genetic <- 50
+    CV_result_genetic <- run_CV_genetic(
+      M_genetic, K_fold_repetitions, K, X_train, Z_train, times_train, delta_train,
+      boost_intercepts_continually=boost_intercepts_continually
+    )
+
+    boosting_type <- "genetic"
+
+
+    ### WRITE CV RESULT TO FILE
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "loglik.csv")
+    write.csv(CV_result_genetic$CV_errors_K_loglik, file=full_filename, row.names=FALSE)
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "deviance.csv")
+    write.csv(CV_result_genetic$CV_errors_K_deviance, file=full_filename, row.names=FALSE)
+
+
+    ## READ CV RESULT FROM FILE
+    seed_string <- formatC(seed, width=2, flag="0")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "loglik.csv")
+    logliks <- read.csv(full_filename)
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "deviance.csv")
+    deviances <- read.csv(full_filename)
+
+    ### POST PROCESSING AND PLOTTING
+    logliks <- CV_result_genetic$CV_errors_K_loglik
+    ylims <- c(min(apply(logliks, 2, min)), max(apply(logliks, 2, max)))
+    m_stop_from_CV <- which.min(rowMeans(logliks))
+    #m_stop_from_CV <- which.max(rowMeans(deviances))
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "loglik.pdf")
+    pdf(full_filename, width=12, height=6)
+    plot(rowMeans(logliks), typ='l', ylim=ylims, ylab="Negative log-likelihood", xlab="Boosting iteration")
+    for (k in 1:K_fold_repetitions) {
+      lines(logliks[, k], lty=3, col=rgb(0, 0, 0, alpha = 0.5))
+    }
+    abline(v=m_stop_from_CV, lwd=2, col='red')
+    legend(
+      'topright',
+      legend=c("Sum of log-lik. on test set in 5-fold CV", "Mean of log-lik. sums in 5-fold CV", "Iteration number which minimizes mean"),
+      col=c(rgb(0, 0, 0, alpha = 0.5), 'black', 'red'),
+      lty=c(3, 1, 1),
+      lwd=c(1, 1, 2)
+    )
+    dev.off()
+
+    result <- cyclic_boosting_run(
+      times=times_train,
+      delta=delta_train,
+      X=X_train,
+      Z=Z_train,
+      m_stop_y0=m_stop_from_CV,
+      m_stop_mu=1,
+      boost_intercepts_continually=boost_intercepts_continually,
+      should_print=FALSE
+    )
+
+    beta_hat <- result$final_parameters$beta_hat_final
+    gamma_hat <- result$final_parameters$gamma_hat_final
+    y0_hat <- exp(X_train %*% beta_hat)
+    mu_hat <- Z_train %*% gamma_hat
+
+    betas <- data.frame(cbind(non_null_parameters(beta_hat) - 1, beta_hat[non_null_parameters(beta_hat)]))
+    names(betas) <- c("j", "beta_j")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "beta.csv")
+    write.csv(betas, file=full_filename, row.names=FALSE)
+    gammas <- data.frame(cbind((1:length(gamma_hat)) - 1, gamma_hat))
+    names(gammas) <- c("j", "gamma_j")
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "gamma.csv")
+    write.csv(gammas, file=full_filename, row.names=FALSE)
+
+
+
+    # Run on test set
+    beta_hat_null <- rep(0, dim(X_test)[2])
+    beta_hat_null[1] <- beta_hat[1]
+    gamma_hat_null <- rep(0, dim(Z_test)[2])
+    gamma_hat_null[1] <- gamma_hat[1]
+    test_null_loglikelihood <- FHT_minus_loglikelihood_with_all_parameters(
+      beta_hat_null, gamma_hat_null, X_test, Z_test, times_test, delta_test
+    )
+    test_loglikelihood <- FHT_minus_loglikelihood_with_all_parameters(
+      beta_hat, gamma_hat, X_test, Z_test, times_test, delta_test
+    )
+    test_difference_of_deviance <- 2*(test_loglikelihood - test_null_loglikelihood)
+    loglikelihood_df <- data.frame(
+      null_loglikelihood=test_null_loglikelihood,
+      loglikelihood=test_loglikelihood,
+      deviance=test_difference_of_deviance
+    )
+    full_filename <- paste0(directory, seed_string, "_", boosting_type, "_", "test_result.csv")
     write.csv(loglikelihood_df, file=full_filename, row.names=FALSE)
   }
 
--- summarize_oberthur.R
+++ summarize_oberthur.R
@@ -1,15 +1,63 @@
 seeds <- 1:99
 directory <- "../dataset/oberthuer/oberthur/"
 seed_strings <- c(formatC(seeds, width=2, flag="0"), '100')
-full_filenames <- paste0(directory, seed_strings, "_", "test_result.csv")
 
+
+full_filenames <- paste0(directory, seed_strings, "_", "test_result.csv")
 full_df <- c()
 for (filename in full_filenames) {
   test_result <- read.csv(filename)
   full_df <- rbind(test_result, full_df)
 }
-full_filename <- '../dataset/oberthuer/deviance_boxplot2.pdf'
+full_deviance <- full_df$deviance
+full_filename <- '../dataset/oberthuer/deviance_boxplot.pdf'
+pdf(full_filename, width=6, height=6)
+boxplot(full_deviance, ylab='Difference in deviance')
+abline(h=0)
+dev.off()
+
+
+full_filenames <- paste0(directory, seed_strings, "_", "beta.csv")
+full_df <- c()
+genes <- rep(0, 9987)
+for (filename in full_filenames) {
+  test_result <- read.csv(filename)
+  chosen <- test_result$j[-1]
+  genes[chosen] <- genes[chosen] + 1
+}
+
+chosen_genes_df <- data.frame(cbind(non_null_parameters(genes), genes[non_null_parameters(genes)]))
+names(chosen_genes_df) <- c("j", "times")
+chosen_genes_df <- chosen_genes_df[order(-chosen_genes_df$times), ]
+top_20_genes <- head(chosen_genes_df, n=20)
+full_filename <- '../dataset/oberthuer/top20_genes.csv'
+write.csv(top_20_genes, file=full_filename, row.names=FALSE)
+
+
+
+
+# CLINICAL
+directory <- "../dataset/oberthuer/oberthur_clinical/"
+full_filenames <- paste0(directory, seed_strings, "_", "clinical_test_result.csv")
+full_df <- c()
+for (filename in full_filenames) {
+  test_result <- read.csv(filename)
+  full_df <- rbind(test_result, full_df)
+}
+clinical_deviance <- full_df$deviance
+full_filename <- '../dataset/oberthuer/deviance_clinical_boxplot.pdf'
 pdf(full_filename, width=6, height=6)
 boxplot(full_df$deviance, ylab='Difference in deviance')
 abline(h=0)
 dev.off()
+
+
+
+
+full_filename <- '../dataset/oberthuer/deviance_both_boxplot.pdf'
+pdf(full_filename, width=12, height=6)
+boxplot(full_deviance, clinical_deviance, xlab='Difference in deviance', horizontal=TRUE)
+axis(2, labels=c("Clinical", "Full"), at=1:2, las=2)
+abline(v=0)
+dev.off()
+
